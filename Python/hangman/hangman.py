a1790accfb114790df8c08d23a8ad5ab5fd1e12a
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2022-11-28 19:35:07 -0800

    modularzation and game building

---
+++ Python/hangman/hangman.py
@@ -0,0 +1,17 @@
+import random
+from words import words
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()

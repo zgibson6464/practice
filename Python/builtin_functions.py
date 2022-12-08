b492bc8b951eb11f76141688c068de17e9d39af8
zgibson6464 <102778567+zgibson6464@users.noreply.github.com>
2022-12-07 19:52:37 -0800

    Create builtin_functions.py
    practice with built in math functions

---
+++ Python/builtin_functions.py
@@ -0,0 +1,16 @@
+print('Math Functions')
+
+abs_int = abs(-1)  # absolute value function
+print(abs_int)
+
+int_to_float = float(100)  # return float value
+print(int_to_float)
+
+float_to_int = int(1.23)  # convert any number into an integer value
+print(float_to_int)
+
+print(int(1.23))
+print(max(1, 2, -5, 10, 0))  # maximum value in a range of numbers
+print(min(1, 2, -5, 10, 0))  # minimum value in a range of numbers
+print(pow(2, 3))  # 2 to the power of 3
+print(round(51.6))  # rounds to the nearest whole integer
